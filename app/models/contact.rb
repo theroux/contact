63ff26cff8237a9ae24a19b5e7188daa5ef10adc
theroux <therouxdown@gmail.com>
2015-12-10 18:49:23 -0500

    Field presence validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
-	
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email_address, presence: true
+	validates :phone_number, presence: true
+	validates :company_name, presence: true
 end
