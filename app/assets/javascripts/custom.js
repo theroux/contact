2da7e3e3961ec4927cfd8a70fd04e231a2d93f55
theroux <therouxdown@gmail.com>
2015-12-22 22:27:02 -0500

    jQuery sorting, filtering, and general UI refinements

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,18 +1,77 @@
-var conctactLite = (function () {
+var contactLite = (function () {
   $(function(){
-  	conctactLite.init();
+  	contactLite.init();
   });
+  // Private vars
+  var $document = $(document),
+    contactClass = 'contact',
+    selectedClass = 'is-selected',
+    $contactsData = $('.data--list'),
+    $deleteLink = $('.delete-contact'),
+    $filterButtons = $('.show-only--button-group'),
+    $allSortButtons = $('.sort-by'),
+    $sortButtons = $('.sort-by--button-group'),
+    $alphaButtons = $('.alpha--button-group');
 
   //Private methods
-  var _bindings = function() {
-    console.log('bindings');
-    var $deleteLink = $('.delete-contact');
-    $deleteLink.on('ajax:success', function(event, xhr, status, error) {
-        console.log('deleted w/ link');
-        $(this).closest('.contact').fadeOut();
+  var _sort = function(param, orderby) {
+    var allContacts =  document.getElementsByClassName(contactClass),
+    contactsArray = [],
+    newHTML = '';
+
+    // getElementsByClassName returns an HTML collection, not a true array, ergo we push to a new array
+    for (var i = 0; i < allContacts.length; i++) {
+      contactsArray.push(allContacts[i]);
+    }
+
+    contactsArray.sort(function(a,b){
+      var keyA = $(a).data(param);
+      var keyB = $(b).data(param);
+
+      if (keyA < keyB) return -1;
+      if (keyA > keyB) return 1;
+      return 0;
     });
-    $deleteLink.on('ajax:error', function(event, xhr, status, error) {
+
+    // Reverse alphabetize if necessary
+    orderby === 'reverse' ? contactsArray.reverse() : null;
+
+    $contactsData.append(contactsArray);
+  },
+  _getAlphaPref = function() {
+    var sortPref = $alphaButtons.find('.' + selectedClass).data('alpha');
+    console.log(sortPref);
+    return sortPref;
+  },
+  _getSortPref = function() {
+    var sortPref = $sortButtons.find('.' + selectedClass).data('sort');
+    console.log(sortPref);
+    return sortPref;
+  },
+  _bindings = function() {
+    // Ajax delete baked into rails/ -- this just handles the UI change
+    $contactsData.on('ajax:success', $deleteLink, function(event, xhr, status, error) {
+        //console.log('Deleted contact w/ link');
+        $(event.target).closest('.' + contactClass).fadeOut();
+    });
+    $contactsData.on('ajax:error', $deleteLink, function(event, xhr, status, error) {
         console.log('Error deleting contact w/ link');
+    });
+    // Filter buttons
+    $filterButtons.on('click', 'button', function(event) {
+      var attr = $(event.target).data('filter');
+      $(event.target).toggleClass(selectedClass);
+      // Change the class only once, not on each individual child contact
+      // Logic for hiding children is easily handled in the CSS
+      $contactsData.toggleClass(attr);
+    });
+    // Sorting buttons
+    $allSortButtons.on('click', 'button', function(event) {
+      $(event.target).addClass(selectedClass).prop("disabled", true).siblings().removeClass(selectedClass).prop("disabled", false);
+      var attr = _getSortPref(),
+      order = _getAlphaPref();
+      console.log(attr, order);
+      _sort(attr, order);
     });
   };
 
@@ -20,6 +79,12 @@ var conctactLite = (function () {
   return {
     init: function () {
       console.log('init');
+
+      // On page load make sure default button is selected
+      // this shows dev intent more clearly than hard coding 'is-selected' on back end
+      $document.find('.default').addClass(selectedClass);
+      $allSortButtons.find('.' + selectedClass).prop("disabled", true);
+
       _bindings();
     }
   };
